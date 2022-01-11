59403678f88284a3b86d5de8ea9f6515620f38cb
Davis Polito <davispolito@dynamic-oit-vapornet100-10-9-89-232.princeton.edu>
2022-01-11 13:35:18 -0500

    First commit for unified backend of Electrosteel and Electrobass

---
+++ Filters.h
@@ -0,0 +1,44 @@
+/*
+  ==============================================================================
+
+    Filters.h
+    Created: 17 Mar 2021 3:10:49pm
+    Author:  Matthew Wang
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "Constants.h"
+#include "Utilities.h"
+
+class ElectroAudioProcessor;
+
+class Filter : public AudioComponent
+{
+public:
+    //==============================================================================
+    Filter(const String&, ElectroAudioProcessor&, AudioProcessorValueTreeState&);
+    ~Filter();
+    
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock);
+    void frame();
+    void tick(float* samples);
+    
+private:
+    
+    void (Filter::*filterTick)(float& sample, int v, float cutoff, float q, float morph);
+    void lowpassTick(float& sample, int v, float cutoff, float q, float morph);
+    void highpassTick(float& sample, int v, float cutoff, float q, float morph);
+    void bandpassTick(float& sample, int v, float cutoff, float q, float morph);
+    
+    //tVZFilter filters[NUM_STRINGS];
+    tSVF lowpass[NUM_STRINGS];
+    tSVF highpass[NUM_STRINGS];
+    tSVF bandpass[NUM_STRINGS];
+    
+    std::atomic<float>* afpFilterType;
+    FilterType currentFilterType = FilterTypeNil;
+};
